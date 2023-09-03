3689e7329660c7f9472aac275eb2887d45baa306
Sambit Sargam Ekalabya <sambitsargam2003@gmail.com>
2023-09-03 19:54:24 +0530

    Completed Sign-in With Google

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -15,7 +15,7 @@ export function Navbar() {
       <div className="navbar-left">
         <Link to="/">
           <h3 className="nav-title" title="Home">
-            Quiz Me
+            NIST MESS FEEDBACK
           </h3>
         </Link>
       </div>
--- src/pages/Auth/SignIn.tsx
+++ src/pages/Auth/SignIn.tsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/Auth/auth-context";
 import "./Auth.css";
+import GoogleButton from 'react-google-button'
+
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const { loginUserWithGoogle, token } = useAuth();
   const navigate = useNavigate();
 
@@ -19,44 +19,10 @@ export function SignIn() {
 
   return (
     <div className="auth-container flex-center">
-      <div className="auth-main-container flex-center">
-        <div className="auth-title">
-          <h2 className="text-center">Sign In</h2>
-        </div>
-        <div className="auth-main">
-          <div className="auth-email">
-            <label htmlFor="mail">Email Address</label>
-            <input
-              placeholder="test@gmail.com"
-              value={email}
-              className="text-input"
-              type="text"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+      <div className=" flex-center">
+      
 
-          <div className="auth-pwd">
-            <label htmlFor="pwd">Password</label>
-            <input
-              placeholder="***********"
-              value={password}
-              className="pwd-input"
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <div className="primary-btn text-center">
-            <button className="link-btn" onClick={loginUserWithGoogle}>
-              Login
-            </button>
-          </div>
-          <div className="auth-secondary-btn text-center">
-            <Link to="/sign-up">
-              Create New Account <i className="fa fa-chevron-right" aria-hidden="true"></i>
-            </Link>
-          </div>
-        </div>
+          <GoogleButton onClick={loginUserWithGoogle}/>
       </div>
     </div>
   );
