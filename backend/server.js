578d879a7ad5e4270131240fcb82c8235965787d
Sambit Sargam Ekalabya <sambitsargam2003@gmail.com>
2023-10-14 02:24:48 +0530

    MInor Edit in Backend

--- backend/server.js
+++ backend/server.js
@@ -47,8 +47,6 @@ app.post("/saveData", async (req, res) => {
         "Content-Type": "application/json",
       },
     });
-    const responsed = await responses.json();
-    console.log(responsed);
   } catch (error) {
     console.error("Error saving data to Firebase:", error.message);
     res.status(500).json({ error: "Failed to save data to Firebase" });
